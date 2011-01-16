ae0e32cc64b40df2258bbbdfcfbbd0a23b15f6f5
Thomas Girard <thomas.g.girard@free.fr>
2011-01-16 13:59:31 +0100

    add licensing information to all Erlang code
    Also change the year in EPLICENSE.

--- src/DsLogAdmin_Common.erl
+++ src/DsLogAdmin_Common.erl
@@ -1,3 +1,29 @@
+%%----------------------------------------------------------------------
+%%
+%% %CopyrightBegin%
+%% 
+%% Copyright Thomas Girard <thomas.g.girard@free.fr> 2011. All Rights Reserved.
+%% 
+%% The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved online at http://www.erlang.org/.
+%% 
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%% 
+%% %CopyrightEnd%
+%%
+%%
+%%----------------------------------------------------------------------
+%% File    : DsLogAdmin_Common.erl
+%% Purpose : Contains validation code used by the log and factory
+%%           implementations.
+%%----------------------------------------------------------------------
+
 -module('DsLogAdmin_Common').
 
 -export([check_full_action/1]).
