d40b0607785b41844eb441a6e53d2cfd4523f108
Thomas Girard <thomas.g.girard@free.fr>
2011-01-18 20:37:34 +0100

    Cosmetic change on copyright information

--- src/DsLogAdmin_Log_impl.erl
+++ src/DsLogAdmin_Log_impl.erl
@@ -2,7 +2,8 @@
 %%
 %% %CopyrightBegin%
 %% 
-%% Copyright Thomas Girard <thomas.g.girard@free.fr> 2011. All Rights Reserved.
+%% Copyright Thomas Girard <thomas.g.girard@free.fr> 2011.
+%% All Rights Reserved.
 %% 
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
